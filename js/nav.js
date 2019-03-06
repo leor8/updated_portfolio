ee51c83360e4f5e5a6db31475210385a65e9c2da
leor <leo442183205@gmail.com>
2019-03-05 18:48:23 -0800

    updating folder structure with nav item animation

--- js/nav.js
+++ js/nav.js
@@ -10,6 +10,22 @@ $(document).ready(function() {
     $(".navTitle").css('color', 'black');
   });
 
+  $(".about").mouseenter(function(event) {
+    navMouseEnter('about');
+  });
+
+  $(".project").mouseenter(function(event) {
+    navMouseEnter('project');
+  });
+
+  $(".about").mouseleave(function(event) {
+    navMouseExit('about')
+  });
+
+  $(".project").mouseleave(function(event) {
+    navMouseExit('project')
+  });
+
 });
 
 // function openNav(event) {
@@ -22,7 +38,21 @@ let openNav = (event) => {
   $('.overlayDivRightBottom').addClass('overlayNavRightBottom');
   $('.overlayDivLeftBottom').addClass('overlayNavLeftBottom');
 
-  $('.overlayForeground').removeClass('overlayForeground').addClass('overlayForegroundDisplay');
+  // This line prevents if the height of the brower changes and the two bottom divs will move and show white spaces.
+  // The settimeout function prevents the callback being ran too early that blocks all the animation
+  setTimeout(function() {
+    $('.overlayDiv').addClass('overlayDivDisplay');
+    $('.overlayForeground').removeClass('overlayForeground').addClass('overlayForegroundDisplay');
+    $('.fading').css('height', '0');
+    $('.fading_reverse').css('height', '0');
+  }, 500);
+
+  // Disable scrolling code from https://stackoverflow.com/questions/3656592/how-to-programmatically-disable-page-scrolling-with-jquery
+  $('html, body').css({
+    overflow: 'hidden',
+    height: '100%'
+  });
+
 }
 
 let closeNav = (event) => {
@@ -31,5 +61,37 @@ let closeNav = (event) => {
   $('.overlayDivRightBottom').removeClass('overlayNavRightBottom');
   $('.overlayDivLeftBottom').removeClass('overlayNavLeftBottom');
 
+  $('.overlayDiv').removeClass('overlayDivDisplay');
+  $('.fading').css('height', '100%');
+  // $('.fading_reverse').css('transform', 'translateY(-100vh)');
+  $('.fading_reverse').css('height', '100%');
+
   $('.overlayForegroundDisplay').removeClass('overlayForegroundDisplay').addClass('overlayForeground');
+
+  // Enable scrolling code from https://stackoverflow.com/questions/3656592/how-to-programmatically-disable-page-scrolling-with-jquery
+  $('html, body').css({
+    overflow: 'auto',
+    height: 'auto'
+  });
 }
+
+// helper functions
+function navMouseEnter(className) {
+  $(".hover_bk.hover_" + className).addClass('hover_bk_show');
+  $("." + className + "_text").css('bottom', '-27rem');
+  $("." + className + "_text").css('padding-bottom', '1rem');
+  $("." + className + "_text").css('border-bottom', '2px solid white');
+}
+
+function navMouseExit(className) {
+  $(".hover_bk.hover_" + className).removeClass('hover_bk_show');
+  $("." + className + "_text").css('bottom', '-30rem');
+  $("." + className + "_text").css('padding-bottom', '0rem');
+  $("." + className + "_text").css('border-bottom', 'none');
+}
+
+
+
+
+
+
