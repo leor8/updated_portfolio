8e51db7dd565d4140f3f06ac64789614e9313059
leor <leo442183205@gmail.com>
2019-03-06 21:39:36 -0800

    adding smooth scrolling effect

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
     imageAsLinkExit();
   });
 
-  $(".return_top_btn").on('click', scrollToSection('click'));
+  smoothScroll();
 });
 
 function imageAsLink() {
@@ -35,10 +35,42 @@ function imageAsLinkExit() {
   $('.hover_image_text').css('display', 'none');
 }
 
-function scrollToSection(event) {
-  event.preventDefault();
-  var $section = $($(this).attr('href'));
-  $('html, body').animate({
-    scrollTop: $section.offset().top
-  }, 500);
+// The code for function below is from https://css-tricks.com/snippets/jquery/smooth-scrolling/ for smooth same page scrolling effects
+function smoothScroll() {
+  // Select all links with hashes
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 }
