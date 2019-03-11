cbffba6aa287151139d98f14722db7876160aa75
leor <leo442183205@gmail.com>
2019-03-10 22:41:15 -0700

    importing project object

--- js/projects.js
+++ js/projects.js
@@ -1,1 +1,74 @@
+$(document).ready(function() {
+  // Loading all projects when page loads
+
+  for(let eachProject in allProjects) {
+    let currProject = allProjects[eachProject];
+
+    let $projectHTML = `<div class="singleProeject ${currProject.projectClassName}" style="background-image: url(${currProject.projectImageURL})">
+                          <div class="project_hover">
+                            <div class="project_title">
+                                <h3> ${currProject.projectName} </h3>
+                            </div>
+
+                            <div class="project_details">
+                              <p class=project_stack> ${stackFormatter(currProject.projectCategory)} </p>
+                              <button class="basic_btn" onclick="openDetail('${eachProject}')">Learn More</button>
+                            </div>
+                          </div>
+                        </div> `;
+
+    $('.project_display').append($projectHTML);
+
+    // Adding hover and mobile touch events when elements have becreated
+    $(`.${currProject.projectClassName}`).mouseenter(function(event) {
+      /* Act on the event */
+      $(`.${currProject.projectClassName} > .project_hover`).toggleClass('hover_project_display');
+    });
+
+    $(`.${currProject.projectClassName}`).on("touchstart", function(event) {
+      /* Act on the event */
+      $(`.${currProject.projectClassName} > .project_hover`).toggleClass('hover_project_display');
+    });
+
+    $(`.${currProject.projectClassName}`).mouseleave(function(event) {
+      $(`.${currProject.projectClassName} > .project_hover`).toggleClass('hover_project_display');
+    });
+
+    $(`.${currProject.projectClassName}`).on("touchend", function(event) {
+      $(`.${currProject.projectClassName} > .project_hover`).toggleClass('hover_project_display');
+    });
+
+  }
+
+
+
+
+});
+
+function openDetail(projectIndex) {
+  let currProject = allProjects[projectIndex];
+
+  $(".project_detail_hide").toggleClass('display_project_detail');
+  $(".project_image").css("background-image", `url("${currProject.projectImageURL}"`);
+}
+
+
+// Helper functions
+function stackFormatter(stackArray) {
+  let returningString = "";
+  stackArray.map(eachStack => {
+    returningString += `${eachStack}, `;
+  })
+
+  return returningString.substring(0, returningString.length-2);
+}
+
+
+
+
+
+
+
+
+
 
--- src/projectsObj.js
+++ src/projectsObj.js
@@ -1,17 +1,19 @@
 let allProjects = {
   0: {
     projectName: "Market Buddy",
+    projectClassName: "marketbuddy",
     projectDescription: "A Smart money saving grocery shopping list maker for everyday use. Our app compares different prices in different grocery stories for the same product. You can construct a shopping list and compare the prices with all grocery stores nearby before you decide where to go for your day to day grocery shops.",
     projectImageURL: "https://github.com/leor8/Market-Buddy-Web/raw/master/docs/Screen%20Shot%202018-08-10%20at%202.04.31%20PM.png",
     projectGitURL: "https://github.com/leor8/Market-Buddy-Web",
     projectStack: ["node.js", "react.js", "scss"],
-    projectCategory: ["frontend, backend"],
+    projectCategory: ["frontend", "backend"],
     projectHTML: "projects/marketBuddy.html"
   } ,
   1: {
     projectName: "Market Buddy Android",
+    projectClassName: "marketbuddyAndroid",
     projectDescription: "A Smart money saving android APP grocery shopping list maker for everyday use. Our app compares different prices in different grocery stories for the same product. You can construct a shopping list and compare the prices with all grocery stores nearby before you decide where to go for your day to day grocery shops.",
-    projectImageURL: "./android_market.png",
+    projectImageURL: "src/android_market.png",
     projectGitURL: "https://github.com/leor8/Market-Buddy-Android",
     projectStack: ["Java"],
     projectCategory: ["mobile"],
@@ -19,6 +21,7 @@ let allProjects = {
   },
   2: {
     projectName: "Ant Racing",
+    projectClassName: "ant",
     projectDescription: "A two player competition Unity2D game that utilize game design theories. ",
     projectImageURL: "https://github.com/leor8/Ant-Racing/raw/master/docs/menu.jpeg?raw=true",
     projectGitURL: "https://github.com/leor8/Ant-Racing-Final",
@@ -28,8 +31,9 @@ let allProjects = {
   },
   3: {
     projectName: "Chatty",
+    projectClassName: "chatty",
     projectDescription: "A realtime communication app built with reactjs and websocket",
-    projectImageURL: "./chatty.png",
+    projectImageURL: "src/chatty.png",
     projectGitURL: "https://github.com/leor8/chattyApp",
     projectStack: ["node.js", "react.js"],
     projectCategory: ["frontend", "backend"],
@@ -46,6 +50,7 @@ let allProjects = {
   // },
   4: {
     projectName: "Tweeter",
+    projectClassName: "tweeter",
     projectDescription: "A twitter clone that tracks all the posts a user makes and save it in MongoDB",
     projectImageURL: "https://github.com/leor8/tweeter/raw/master/docs/WechatIMG694.jpeg?raw=true",
     projectGitURL: "https://github.com/leor8/tweeter",
@@ -55,6 +60,7 @@ let allProjects = {
   },
   5: {
     projectName: "ToDoBot",
+    projectClassName: "todo",
     projectDescription: "Todo Bot is a SMART todo list to keep track of restaurants, movies, books or purchases you want to keep track of",
     projectImageURL: "https://github.com/leor8/midterm_todo_list/raw/master/Screenshots/Index.JPG",
     projectGitURL: "https://github.com/leor8/midterm_todo_list",
